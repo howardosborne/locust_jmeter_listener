c2cd13f2b1c9a44f38533ff8e6a08e1c2355b821
Howard Osborne <hosborne@etesting.com>
2020-05-08 16:41:12 +0100

    changed millisecond calculation for v1.0

--- system_under_test/fake.js
+++ system_under_test/fake.js
@@ -4,6 +4,7 @@ const http = require(config.protocol);
 const requestListener = function (req, res) {
   res.writeHead(config.responseCode, config.responseHeaders);
   res.end(config.responseMessage);
+  console.log("got one")
 }
 
 const server = http.createServer(requestListener);
